fe97bfd7cedecef46ffa85f5883c0042099253e3
sasha53545 <96.kazzak.96@gmail.com>
2019-08-30 22:34:51 +0300

    q

---
+++ Interview_Preparation_Kit/Arrays/2D_Array-DS/2D_Array-DS.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the hourglassSum function below.
+function hourglassSum(arr) {
+        var maxRow = 3;
+        var maxColumn = 3;
+        var result = [];
+        for (var i = 0; i <= maxColumn; i++) {
+            for (var j = 0; j <= maxRow; j++) {
+                result.push(arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
+            }
+        }
+        result.sort((a, b) => b - a);
+        console.log(result[0]);
+        return result[0];
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    let result = hourglassSum(arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
---
+++ Interview_Preparation_Kit/Arrays/Arrays:_Left_Rotation/Arrays:_Left_Rotation.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the rotLeft function below.
+function rotLeft(a, d) {
+    var tmp = 0;
+    while (d > 0) {
+        for (var i = 0; i < a.length - 1; i++) {
+            tmp = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = tmp;
+        }
+        d--;
+    }
+    console.log(a);
+    return a;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = rotLeft(a, d);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
